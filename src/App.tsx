fea794b54943cbf0943d7b1e601ba02d66edc41a
lukelee <lukelee01@gmail.com>
2020-03-11 17:14:00 -0400

    install reactstrap and update App.tsx.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
